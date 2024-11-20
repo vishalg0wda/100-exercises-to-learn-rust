3979f258d67d6267a8fded8f3fb15b1d8c5fe16e
Vishal Gowda <cartmanboy1991@gmail.com>
2024-11-20 10:25:39 +0000

    Complete exercises 03_ticket_v1 modules to outro

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,50 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u8,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u8, unit_price: u32) -> Self {
+        if product_name.is_empty() || product_name.len() > 300 || unit_price == 0 || quantity == 0 {
+            panic!();
+        }
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u8 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity as u32 * self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        self.product_name = product_name
+    }
+
+    pub fn set_quantity(&mut self, quantity: u8) {
+        self.quantity = quantity
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        self.unit_price = unit_price
+    }
+}
